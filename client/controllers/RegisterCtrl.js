206340c77cf3b67fd3c45f6acf29e0b704adb6ce
tchamberlain <taylorannchamberlain@gmail.com>
2016-07-07 10:48:47 -0700

    add chart to weather controller

--- client/controllers/RegisterCtrl.js
+++ client/controllers/RegisterCtrl.js
@@ -1,19 +1,6 @@
 angular.module('RegisterCtrl', []).controller('registerController',
   ['$scope', '$location', 'AuthService',
   function ($scope, $location, AuthService) {
-
-    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-       $scope.series = ['High Temp', 'Low Temp'];
-       $scope.data = [
-         [65, 59, 80, 81, 56, 55, 40],
-         [28, 48, 40, 19, 86, 27, 90]
-       ];
-       $scope.onClick = function (points, evt) {
-         console.log(points, evt);
-       };
-
-
-
     $scope.switchForms = function () {
           $location.path('/login');
         }
--- client/controllers/WeatherCtrl.js
+++ client/controllers/WeatherCtrl.js
@@ -2,6 +2,16 @@ angular.module('WeatherCtrl',[])
 .controller('weatherController',['$scope','WeatherSearchService','$timeout',
   function ( $scope, WeatherSearchService, $timeout, $q ) {
    
+  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+     $scope.series = ['High Temp', 'Low Temp'];
+     $scope.data = [
+       [65, 59, 80, 81, 56, 55, 40],
+       [28, 48, 40, 19, 86, 27, 90]
+     ];
+     $scope.onClick = function (points, evt) {
+       console.log(points, evt);
+     };
+
     $scope.searchPlace = function( placeEntry ){
       // use service to generate google places autocomplete predictions
       WeatherSearchService.getPlaceData( placeEntry )
