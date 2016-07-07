e383a93883f60591ac394403668321eb65224062
tchamberlain <taylorannchamberlain@gmail.com>
2016-07-07 15:03:21 -0700

    Change logout service

---
+++ client/directives/WeatherBlockDir.js
@@ -0,0 +1,12 @@
+angular.module('WeatherBlockDir',[])
+.directive('weatherBlock', function() {
+     return {
+      restrict:'E',
+      scope: {
+         weather: '=weather'
+      },
+        templateUrl: './views/weatherBlock.html',
+    }
+});
+
+
