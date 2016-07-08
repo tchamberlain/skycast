57de72eb99545244ad564b56fa8034f565d39163
tchamberlain <taylorannchamberlain@gmail.com>
2016-07-07 17:01:41 -0700

    put user functions into controller

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+var UserController = require('../user/userController.js');
 var User = require('../user/userModel.js');
 var request = require('request');
 var darkskyKey = process.env.darkskyKey || require("../config").darkskyKey;
@@ -10,77 +11,15 @@ var places = new GooglePlaces( googlePlacesKey );
 var Q = require('q');
 var findUser = Q.nbind(User.findOne, User);
 
-router.post('/user/register', function(req, res) {
-  User.register(new User({ username: req.body.username }),
-    req.body.password, function(err, account) {
-    if (err) {
-      return res.status(500).json({
-        err: err
-      });
-    }
-    passport.authenticate('local')(req, res, function () {
-      return res.status(200).json({
-        status: 'Registration successful!',
+router.post('/user/register', UserController.register);
 
-      });
-    });
-  });
-});
+router.post('/user/login', UserController.login);
 
-router.post('/user/login', function(req, res, next) {
-  passport.authenticate('local', function(err, user, info) {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return res.status(401).json({
-        err: info
-      });
-    }
-    req.logIn(user, function(err) {
-      if (err) {
-        return res.status(500).json({
-          err: 'Could not log in user'
-        });
-      }
-      res.status(200).json({
-        status: 'Login successful!'
-      });
-    });
-  })(req, res, next);
-});
+router.get('/user/logout', UserController.logout);
 
-router.get('/user/logout', function(req, res) {
-  req.logout();
-  res.status(200).json({
-    status: 'Bye!'
-  });
-});
+router.get('/user/pastSearches', UserController.pastSearches);
 
-router.get('/user/pastSearches', function(req, res) {
-    User.findOne({_id: req.user._id })
-    .then(function (user) {
-      if (user) {
-        res.json({
-          username: user.username,
-          pastSearches: user.pastSearches
-        });
-      } else {
-        console.error('Error finding users');
-      }
-    });
-});
-
-router.get('/user/status', function(req, res) {
-  if (!req.isAuthenticated()) {
-    return res.status(200).json({
-      status: false
-    });
-  }
-  res.status(200).json({
-    status: true
-  });
-});
+router.get('/user/status', UserController.status);
 
 router.post('/placeData', function(req, res) {
   places.autocomplete({ input: req.body.place }, function(err, response) {
---
+++ server/user/userController.js
@@ -0,0 +1,81 @@
+var User = require('../user/userModel.js');
+var Q = require('q');
+var findUser = Q.nbind(User.findOne, User);
+var passport = require('passport');
+
+
+module.exports = {
+
+  register: function(req, res) {
+    User.register(new User({ username: req.body.username }),
+      req.body.password, function(err, account) {
+      if (err) {
+        return res.status(500).json({
+          err: err
+        });
+      }
+      passport.authenticate('local')(req, res, function () {
+        return res.status(200).json({
+          status: 'Registration successful!',
+        });
+      });
+    });
+  },
+
+  login: function(req, res, next) {
+  passport.authenticate('local', function(err, user, info) {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.status(401).json({
+        err: info
+      });
+    }
+    req.logIn(user, function(err) {
+      if (err) {
+        return res.status(500).json({
+          err: 'Could not log in user'
+        });
+      }
+      res.status(200).json({
+        status: 'Login successful!'
+    });
+    });
+  })(req, res, next);
+  },
+
+  logout: function(req, res) {
+    req.logout();
+    res.status(200).json({
+      status: 'Bye!'
+    });
+  },
+
+  pastSearches: function(req, res) {
+    User.findOne({_id: req.user._id })
+    .then(function (user) {
+      if (user) {
+        res.json({
+          username: user.username,
+          pastSearches: user.pastSearches
+        });
+      } else {
+        console.error('Error finding users');
+      }
+    });
+  },
+  
+  status: function(req, res) {
+    if (!req.isAuthenticated()) {
+      return res.status(200).json({
+        status: false
+      });
+    }
+    res.status(200).json({
+      status: true
+    });
+  }
+
+
+}
